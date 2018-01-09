0e755a7355782355149484c7eb50a5e35ee4b9bd
was.li <wasuwat.se@gmail.com>
2018-01-09 15:38:01 +0700

    init node express back-end

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+var request = require('request');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'api endpoint'
+    });
+});
+
+app.get('/getAccessToken', (req, res) => {
+    request.post({
+        url: 'https://api.medium.com/v1/tokens',
+        form: {
+            // Dynamic
+            code: req.query.code,
+            client_id: '5425e5ae4e52',
+            client_secret: '65f35242d392cc32a695e2ba98e575fee7079cd3',
+            grant_type: 'authorization_code',
+            redirect_uri: 'http://127.0.0.1:3000'
+        }
+    }, (err, httpResponse, body) => {
+        res.send(body);
+        console.log(body);
+    })
+});
+
+app.get('/getUserDetail', (req, res) => {
+    request.get({
+        url: 'https://api.medium.com/v1/me',
+        headers: {
+            // Dynamic
+            'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+    }, (err, httpResponse, body) => {
+        res.send(body);
+        console.log(body);
+    })
+})
+
+app.get('/listPublications', (req, res) => {
+    request.get({
+        // Dynamic
+        url: 'https://api.medium.com/v1/users/1af639ff52d2f8e1aa52ee42b42e211a6f5a2d05dadf2a0ef8991173bd4f6075f/publications',
+    }, (err, httpResponse, body) => {
+        res.send(body);
+        console.log(body);
+    })
+})
+
+
+app.listen(PORT, (data) => {
+    console.log('server start on port ' + PORT);
+});
