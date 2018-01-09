ed01d799a7d285c8c842c70580b3b8915abd2e79
was.li <wasuwat.se@gmail.com>
2018-01-09 16:45:07 +0700

    get access_token

--- angular-source/src/app/component/login/login.component.ts
+++ angular-source/src/app/component/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  scope = 'basicProfile,listPublications';
+  // tslint:disable-next-line:max-line-length
+  REQUEST_TEMP_AUTH_CODE_API = 'https://medium.com/m/oauth/authorize?' + 'client_id=' + environment.API_SECRET.clientId + '&scope=' + this.scope + '&state=state&response_type=code&redirect_uri=' + environment.redirectUri;
+
+  constructor(private http: HttpClient, private router: Router, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.activatedRoute.queryParams.subscribe(params => {
+      if (params['code']) {
+        this.requestAccessToken(params['code']);
+        console.log('Has temporary code');
+      }
+    });
+  }
+
+  requestTemporaryAuthCode() {
+    const parentWindow = window.open(this.REQUEST_TEMP_AUTH_CODE_API, '_blank');
+    // TODO
+    window.close();
+  }
+
+  requestAccessToken(code) {
+    this.http.get(environment.API_BASE_URL + '/getAccessToken?code=' + code).subscribe(data => {
+      console.log(data.access_token);
+      localStorage.setItem('access_token', data.access_token);
+    });
   }
 
 }
