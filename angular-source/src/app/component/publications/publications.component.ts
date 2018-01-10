c649b4591a01487dc46842e1c9cc80bb161f21ab
was.li <wasuwat.se@gmail.com>
2018-01-10 12:41:44 +0700

    Display publications in material designed cards

--- angular-source/src/app/component/publications/publications.component.ts
+++ angular-source/src/app/component/publications/publications.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PublicationService } from '../../service/publication.service';
 
 @Component({
   selector: 'app-publications',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PublicationsComponent implements OnInit {
 
-  constructor() { }
+  publications: any;
 
-  ngOnInit() {
+  constructor(
+    public publicationSerice: PublicationService
+  ) {
   }
 
+  ngOnInit() {
+    this.publicationSerice.getPublications();
+    this.publications = JSON.parse(localStorage.getItem('publicationsList'));
+  }
 }
