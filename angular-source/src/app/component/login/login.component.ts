5f516005a0d72b094dbfba0404354349ae41121c
was.li <wasuwat.se@gmail.com>
2018-01-10 12:27:22 +0700

    Fix routing

--- angular-source/src/app/component/login/login.component.ts
+++ angular-source/src/app/component/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
+
+import { AuthService } from '../../service/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -12,29 +14,42 @@ export class LoginComponent implements OnInit {
   scope = 'basicProfile,listPublications';
   // tslint:disable-next-line:max-line-length
   REQUEST_TEMP_AUTH_CODE_API = 'https://medium.com/m/oauth/authorize?' + 'client_id=' + environment.API_SECRET.clientId + '&scope=' + this.scope + '&state=state&response_type=code&redirect_uri=' + environment.redirectUri;
+  temporaryAuthCode: string;
 
-  constructor(private http: HttpClient, private router: Router, private activatedRoute: ActivatedRoute) { }
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
-    this.activatedRoute.queryParams.subscribe(params => {
-      if (params['code']) {
-        this.requestAccessToken(params['code']);
-        console.log('Has temporary code');
+    if (!this.temporaryAuthCode) {
+      console.log('There is no access token');
+      this.extractTemporaryCode();
+      this.authService.requestAccessToken(this.temporaryAuthCode);
+
+      if (localStorage.getItem('currentUser')) {
+        this.router.navigate(['/profile']);
       }
+    } else {
+      this.router.navigate(['/profile']);
+    }
+  }
+
+  extractTemporaryCode() {
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.temporaryAuthCode = params['code'];
     });
   }
 
-  requestTemporaryAuthCode() {
+  askMediumPermission() {
+    // TODO : Check if access_token already exist
     const parentWindow = window.open(this.REQUEST_TEMP_AUTH_CODE_API, '_blank');
-    // TODO
+    // TODO : close child window
     window.close();
   }
 
-  requestAccessToken(code) {
-    this.http.get(environment.API_BASE_URL + '/getAccessToken?code=' + code).subscribe(data => {
-      console.log(data.access_token);
-      localStorage.setItem('access_token', data.access_token);
-    });
-  }
+
 
 }
