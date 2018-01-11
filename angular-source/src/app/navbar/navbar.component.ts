47aed8a468e0193ca248f0fa35a68c0aa3f4a3af
was.li <wasuwat.se@gmail.com>
2018-01-11 11:17:46 +0700

    Demo navbar

---
+++ angular-source/src/app/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../service/auth.service';
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor(private authSerice: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  logout() {
+    this.authSerice.logout();
+  }
+}
