d4441a8765fba0f4aad7136598e77efabb7f826f
was.li <wasuwat.se@gmail.com>
2018-01-12 14:23:03 +0700

    does not need oninit

--- angular-source/src/app/navbar/navbar.component.ts
+++ angular-source/src/app/navbar/navbar.component.ts
@@ -1,18 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AuthService } from '../service/auth.service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
 
   didUserLoggedIn = localStorage.getItem('accessToken') ? true : false;
 
   constructor(private authSerice: AuthService) { }
-
-  ngOnInit() {
-  }
 
   logout() {
     this.authSerice.logout();
