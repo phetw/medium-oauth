dbd823d34d89d5d2d37c660b0ff6e90d5f03ed88
was.li <wasuwat.se@gmail.com>
2018-01-15 17:58:43 +0700

    remove nested subscription and unsubscribe on destroy

--- angular-source/src/app/component/login/login.component.ts
+++ angular-source/src/app/component/login/login.component.ts
@@ -1,56 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
-
 import { AuthService } from '../../service/auth.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
-  scope = 'basicProfile,listPublications';
-  // tslint:disable-next-line:max-line-length
-  REQUEST_TEMP_AUTH_CODE_API = 'https://medium.com/m/oauth/authorize?' + 'client_id=' + environment.API_SECRET.clientId + '&scope=' + this.scope + '&state=state&response_type=code&redirect_uri=' + environment.redirectUri;
-  urlContainsTempCode: Boolean = false;
+  tempAuthCode: String = '';
+  public subscribers: any = {};
 
   constructor(
-    private http: HttpClient,
     private router: Router,
     private activatedRoute: ActivatedRoute,
     private authService: AuthService,
   ) { }
 
-
-
   ngOnInit() {
     this.checkIfUrlContainsTempCode();
 
-    if (this.urlContainsTempCode) {
-      this.activatedRoute.queryParams.subscribe(params => {
-        this.authService.requestAccessToken(params['code']).subscribe(accessToken => {
-          this.authService.getUserProfile(accessToken).subscribe((user) => {
-            this.router.navigate(['/publications']);
-          });
+    if (this.tempAuthCode) {
+      this.subscribers.token = this.authService.requestAccessToken(this.tempAuthCode).subscribe(accessToken => {
+        this.subscribers.profile = this.authService.getUserProfile(accessToken).subscribe(() => {
+          this.router.navigate(['/publications']);
         });
       });
-    } else {
-      // Do nothing until user sign in with Medium
     }
   }
 
   checkIfUrlContainsTempCode() {
     this.activatedRoute.queryParams.subscribe(params => {
       if (params['code']) {
-        this.urlContainsTempCode = true;
+        this.tempAuthCode = params['code'];
       }
     });
   }
 
   askMediumPermission() {
-    const parentWindow = window.open(this.REQUEST_TEMP_AUTH_CODE_API, '_blank');
+    const parentWindow = window.open(environment.GET_TEMP_CODE, '_blank');
     window.close();
+  }
+
+  ngOnDestroy() {
+    // Prevent memory leaks
+    this.subscribers.token.unsubscribe();
+    this.subscribers.profile.unsubscribe();
   }
 }
