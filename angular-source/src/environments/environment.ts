35d1ebdd93357fd58db6dcff043de0086491392b
was.li <wasuwat.se@gmail.com>
2018-01-15 18:00:54 +0700

    update environment file

--- angular-source/src/environments/environment.ts
+++ angular-source/src/environments/environment.ts
@@ -6,9 +6,12 @@
 export const environment = {
   production: false,
   API_BASE_URL: 'http://127.0.0.1:3000',
-  API_SECRET: {
-    clientId: '5425e5ae4e52',
-    clientSecret: '65f35242d392cc32a695e2ba98e575fee7079cd3',
-  },
+  clientId: '5425e5ae4e52',
+  clientSecret: '65f35242d392cc32a695e2ba98e575fee7079cd3',
   redirectUri: 'http://127.0.0.1:4200/login',
+  scope: 'basicProfile,listPublications',
+  state: 'state',
+  responseType: 'code',
+  // tslint:disable-next-line:max-line-length
+  GET_TEMP_CODE: 'https://medium.com/m/oauth/authorize?client_id=5425e5ae4e52&scope=basicProfile,listPublications&state=state&response_type=code&redirect_uri=http://127.0.0.1:4200/login',
 };
